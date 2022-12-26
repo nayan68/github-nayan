d05343bba794c1e9de53d4a0bbea953c22c0411c
nayan68 <94389609+nayan68@users.noreply.github.com>
2022-12-26 19:59:43 +0530

    Create tempCodeRunnerFile.c

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+1.	#include <stdio.h>  
+2.	int main()  
+3.	{  
+4.	    int age;  // variable declaration  
+5.	    printf("Enter your age");
+       // printf("Your Gender")  
+6.	    scanf("%d",&age);   // taking user input for age variable
+       // scanf("%c",&gender);  
+7.	    (age>=18) ? (printf("eligible for voting")) : (printf("not eligible for voting"));  // conditional operator 
+       // (gender male || female) ? (printf("Your Male")) : (printf("Your Female"));
+8.	    return 0;  
+9.	}  
